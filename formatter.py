7e3cbae8d7a0ae2c05468cbbc971089c6e94c42e
Matthew Wang <matt@matthewwang.me>
2019-06-15 23:07:30 -0700

    reviving things from the dead

--- formatter.py
+++ formatter.py
@@ -28,15 +28,26 @@ trash = [
     "mtv unlugged",
     "intro",
     "interlude",
-    "freestyle"
+    "freestyle",
+    'interview',
+    'Q&A',
+    'comments',
+    'tradução',
+    'türkçe',
+    'performance',
+    'studio',
+    'statement',
+    'vh1',
+    'grammy',
+    'grammys'
 ]
 
-output = []
+things_to_remove = dict.fromkeys(map(ord, '()[]'), None)
 
 def format_songs(lyrics_object):
     output_object = []
     for i in range(len(lyrics_object["songs"])):
-        if any(x in lyrics_object["songs"][i]["title"].lower() for x in trash):
+        if any(x in lyrics_object["songs"][i]["title"].lower().translate(things_to_remove) for x in trash):
             print("Found naughty song title, popping " + lyrics_object["songs"][i]["title"])
         else:
             print("Adding "  + lyrics_object["songs"][i]["title"])
@@ -48,14 +59,16 @@ with open("artists.json") as f:
 
 for genre in artists["genres"]:
     for i in range(len(artists["genres"][genre])):
+        output = []
         if (os.path.isfile("lyrics/" + artists["genres"][genre][i] +".json")):
             with open("lyrics/" + artists["genres"][genre][i] +".json") as f:
                 lyrics = json.load(f)
                 #output.append(format_songs(lyrics))
                 output += format_songs(lyrics)
+            with open("lyrics/" + artists["genres"][genre][i] + ".txt", "w") as f:
+                for item in output:
+                    f.write(item)
         else:
             print("Missing artist info")
 
-with open("output.txt", "w") as f:
-    for item in output:
-        f.write(item)
+
